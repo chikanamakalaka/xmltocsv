fa08fea49b1c3fbe7dc680634ddd56647b577f9e
Matthew Hocker <mhocker@gmail.com>
2018-07-12 17:04:52 -0700

    fixes

--- scrape.py
+++ scrape.py
@@ -2,7 +2,29 @@
 
 from lxml import html
 import requests
-page = requests.get('video.html')
+import os
+from requests_testadapter import Resp
+
+class LocalFileAdapter(requests.adapters.HTTPAdapter):
+    def build_response_from_file(self, request):
+        file_path = request.url[7:]
+        with open(file_path, 'rb') as file:
+            buff = bytearray(os.path.getsize(file_path))
+            file.readinto(buff)
+            resp = Resp(buff)
+            r = self.build_response(request, resp)
+
+            return r
+
+    def send(self, request, stream=False, timeout=None,
+             verify=True, cert=None, proxies=None):
+
+        return self.build_response_from_file(request)
+
+requests_session = requests.session()
+requests_session.mount('file://', LocalFileAdapter())
+
+page = requests_session.get('file:///Users/mhocker/Desktop/xmltocsv/video.html')
 tree = html.fromstring(page.content)
 
 #Get Section headers
